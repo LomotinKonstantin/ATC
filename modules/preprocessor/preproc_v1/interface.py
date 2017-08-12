ece12ae7a689532c8cd2ac080d70b2b329a26706
LomotinKonstantin <ke.lomotin@gmail.com>
2017-08-12 15:23:36 +0300

    Добавлена загрузка стоп-слов
    Signed-off-by: LomotinKonstantin <ke.lomotin@gmail.com>

--- modules/preprocessor/preproc_v1/interface.py
+++ modules/preprocessor/preproc_v1/interface.py
@@ -19,8 +19,8 @@ class Preprocessor(Module):
         self._load_vocabulary()
 
     def _load_vocabulary(self):
-        # loading VINITI alphabet
-        alph = open(".vocabulary/viniti_alphabet.txt").read().split("\n")
+        # Loading VINITI alphabet
+        alph = open("./vocabulary/viniti_alphabet.txt").read().split("\n")
         alph = list(filter(lambda a: a != "", alph))
         rules = {}
         for i in alph:
@@ -43,4 +43,36 @@ class Preprocessor(Module):
         #               "форм. доп.",
         #               "диакр. наезж."], axis=1, inplace=True)
         self.alphabet = [i for j in alph_df.values.flatten() for i in j]
-        #
+        # Loading stop-words
+        self.stop_words = open(".vocabulary/stop_words.txt", encoding="utf-8").read().split("\n")
+        self.stop_words = list(filter(lambda a: a != "", self.stop_words))
+
+    def process(self, text):
+        """ Processing some text. Removes tokens depending on 'mode' selected. Lemmatization included
+
+            Parameters
+            ----------
+            text: some text to process
+
+            Returns
+            -------
+            Clear from VINITI`s alphabet and lemmatized text
+
+        """
+        fin = "###"
+        s = text.strip()
+        if s.contains("_Ё"):
+            s.remove("_Ё")
+        s = s.lower()
+        s = re.sub(self.email_re, "", s)
+        for i in self.chars:
+            s = s.replace(i, "")
+        s = re.sub("\s+", " ", s)
+        s = re.sub(self.parse_re, "", s)
+        print("Lemmatization...")
+        s = "".join(self.lemm.lemmatize(s))
+        print("Removing redundant \"-\"")
+        s = re.sub("(((?<=^)|(?<=\w))-+((?=\s)|(?=$)))|(((?<=^)|(?<=\s))-+((?=\w)|(?=\s)|(?=$)))", "", s)
+        print("Removing remainings")
+        s = re.sub("(^|(?<=\s))\w{,2}(?=\s|$)", "", s)
+        return " ".join(list(filter(lambda a: a != "", s.split())))
