9a766395e2487d4140239c42ff2739bcd9f4c385
KozlovaES <Temari11@mail.ru>
2017-08-21 19:19:47 +0300

    Добавлены сигналы ошибок в модуль классификатора.
    Signed-off-by: KozlovaES <Temari11@mail.ru>

--- modules/classifier/classifier_v1/interface.py
+++ modules/classifier/classifier_v1/interface.py
@@ -17,17 +17,25 @@ class Classifier(Module):
         
     def classify(self, vector):
         if self.clf:
-            result = pd.Series(self.clf.predict_proba([vector])[0], index=self.clf.classes_)
-            result = result.sort_values(ascending=False)
-            # result = result.round(3)
-            # result = result[result!=0]
-            return result   
+            if clf.coef_.T.shape[0] == len(vector):
+                result = pd.Series(self.clf.predict_proba([vector])[0], index=self.clf.classes_)
+                result = result.sort_values(ascending=False)
+                # result = result.round(3)
+                # result = result[result!=0]
+                return result   
+            else:
+                self.error_occurred.emit('Vector has '+str(len(vector))+' elements. Model can work with vectors that have '+str(clf.coef_.T.shape[0])+'attibutes.')
+                return None
         else:
             return None        
         
     def loadConfig(self): 
         configParcer = ConfigParser()
-        configParcer.read(os.path.dirname(__file__) + '/config.ini')
+        file = os.path.dirname(__file__) + '/config.ini'
+        if os.path.exists(file):
+            configParcer.read(file)
+        else:
+            self.error_occurred.emit("Can't find the configuration file")
         return configParcer    
     
     def loadClf(self):
@@ -36,4 +44,5 @@ class Classifier(Module):
         if os.path.exists(file):
             self.clf = joblib.load(file)
         else:
+            self.error_occurred.emit("The classifier does not exists.")
             self.clf = None
