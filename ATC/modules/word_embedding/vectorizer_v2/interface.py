f132c25651183565421da6cb5fa354264b7f1ba6
KozlovaES <hse.kozlovaes@gmail.com>
2017-11-07 17:04:58 +0100

    Добавлена поддержка английского языка и создание вектора по средн.зн.
    Добавлены классификаторы и векторайзер для работы с английским языком.
    Изменена структура приложения, теперь возможно задавать язык перед
    классификацией.
    Добавлена возможность создавать векторы понижением размерности матрицы
    по среднему значению.
    
    Signed-off-by: KozlovaES <hse.kozlovaes@gmail.com>

---
+++ ATC/modules/word_embedding/vectorizer_v2/interface.py
@@ -0,0 +1,76 @@
+from gensim.models import Word2Vec
+from configparser import ConfigParser
+import numpy as np
+import os.path
+import sys 
+from modules.common import Module
+
+
+class WordEmbedding(Module):
+    def __init__(self, lang='ru'):
+        super().__init__(os.path.dirname(__file__) + "\\metadata.json")
+        self.lang = lang
+        self.config = self.loadConfig()
+        self.loadModel()
+        
+    # Creating vector according to last set language.
+    # Language can be set in constructor, vectorize(self, text, lang) function
+    # or with setter.
+    def vectorize(self, text):
+        if self.model:
+            tokens = text.split()
+            features = [0]*self.length
+            if self.config.get('Settings', 'convolution') == 'sum':
+                for t in tokens:
+                    if t in self.model:
+                        features += self.model[t]
+            elif self.config.get('Settings', 'convolution') in ['max','mean']:
+                for t in tokens:
+                    if t in self.model:
+                        features = np.vstack((features, self.model[t]))
+                if features.shape[0] > 1:
+                    if self.config.get('Settings', 'convolution') == 'max':
+                        features = features.max(axis=0)
+                    else:
+                        features = features.mean(axis=0)
+                features = [features]              
+            return features[0]
+        else:
+            return None
+        
+    # Checks if input language corresponds with model language before creating vectors.
+    # If it does not, set new model language and changes model.
+    def vectorize(self, text, lang):
+        if self.lang != lang:
+            self.lang = lang
+            self.loadModel()
+        return self.vectorize(text)
+        
+    def loadModel(self):
+        file = os.path.join(os.path.dirname(__file__), self.config.get('Settings', self.lang))
+        if os.path.exists(file):
+            self.model = Word2Vec.load(file)
+            self.length = self.model.layer1_size
+        else:
+            self.error_occurred.emit('The model does not exist.')
+            self.model = None
+
+    def loadConfig(self): 
+        configParcer = ConfigParser()
+        file = os.path.dirname(__file__) + '/config.ini'
+        if os.path.exists(file):
+            configParcer.read(file)
+        else:
+            self.error_occurred.emit("Can't find the configuration file.")
+        return configParcer
+
+    def rejectThreshold(self):
+        return self.config.get("Settings", "reject_threshold")
+    
+    # Language setter. Reloads model immediately.
+    def setLang(lang):
+        self.lang = lang
+        self.loadModel()
+        
+    def getLang():
+        return self.lang
