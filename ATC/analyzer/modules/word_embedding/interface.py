758149686ff854acdcf114fa2e19907f1226fe6d
Konstantin Lomotin <ke.lomotin@gmail.com>
2018-01-17 15:14:07 +0100

    Модуль векторайзера сделан многоразовым
    Signed-off-by: Konstantin Lomotin <ke.lomotin@gmail.com>

--- ATC/analyzer/modules/word_embedding/interface.py
+++ ATC/analyzer/modules/word_embedding/interface.py
@@ -7,30 +7,30 @@ from analyzer.modules.module import Module
 
 
 class WordEmbedding(Module):
-    def __init__(self, lang='ru'):
+    def __init__(self):
         super().__init__(os.path.join(os.path.dirname(__file__), "metadata.json"))
-        self.lang = lang
+        self.last_lang = None
+        self.model = None
         self.config = self.loadConfig()
-        self.loadModel()
-        
+
     # If lang == None, creates vector according to last set language.
     # If lang != None, checks if input language corresponds with model language 
     # before creating vectors. If it does not, set new model language and changes model.
     # Language can be set in constructor, vectorize(self, text, lang) function
     # or with setter.
-    def vectorize(self, text, lang=None):
+    def vectorize(self, text, lang):
         if lang:
-            if self.lang != lang:
-                self.lang = lang
+            if self.last_lang != lang:
+                self.last_lang = lang
                 self.loadModel()            
         if self.model:
             tokens = text.split()
-            features = [0]*self.length
+            features = [0] * self.length
             if self.config.get('Settings', 'convolution') == 'sum':
                 for t in tokens:
                     if t in self.model:
                         features += self.model[t]
-            elif self.config.get('Settings', 'convolution') in ['max','mean']:
+            elif self.config.get('Settings', 'convolution') in ['max', 'mean']:
                 for t in tokens:
                     if t in self.model:
                         features = np.vstack((features, self.model[t]))
@@ -44,12 +44,12 @@ class WordEmbedding(Module):
             return None
         
     def loadModel(self):
-        file = os.path.join(os.path.dirname(__file__), self.config.get('Settings', self.lang))
+        file = os.path.join(os.path.dirname(__file__), self.config.get('Settings', self.last_lang))
         if os.path.exists(file):
             self.model = Word2Vec.load(file)
             self.length = self.model.layer1_size
         else:
-            self.error_occurred.emit('The model does not exist.')
+            self.error_occurred.emit('The WE model does not exist.')
             self.model = None
 
     def rejectThreshold(self):
@@ -57,8 +57,8 @@ class WordEmbedding(Module):
     
     # Language setter. Reloads model immediately.
     def setLang(self, lang):
-        self.lang = lang
+        self.last_lang = lang
         self.loadModel()
         
     def getLang(self):
-        return self.lang
+        return self.last_lang
