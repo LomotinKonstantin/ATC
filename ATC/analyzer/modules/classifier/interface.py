79690142c3b2154579b6c7cb9cb4396364c3bc58
Konstantin Lomotin <ke.lomotin@gmail.com>
2018-01-17 15:13:50 +0100

    Модуль классификатора сделан многоразовым
    Signed-off-by: Konstantin Lomotin <ke.lomotin@gmail.com>

--- ATC/analyzer/modules/classifier/interface.py
+++ ATC/analyzer/modules/classifier/interface.py
@@ -5,22 +5,23 @@ from sklearn.externals import joblib
 
 
 class Classifier(Module):
-    def __init__(self, rubr_id='SUBJ', lang='ru'):
+    def __init__(self):
         super().__init__(os.path.join(os.path.dirname(__file__), "metadata.json"))
-        self.rubr_id = rubr_id
-        self.lang = lang
+        self.last_rubr_id = None
+        self.last_lang = None
+        self.clf = None
         self.config = self.loadConfig()
-        self.clf = self.loadClf()
 
     # If lang == None, classifies vector according to last set language.
     # If lang != None, checks if input language corresponds with current language 
     # before classification. If it does not, set new current language and changes classifier.       
     # Language can be set in constructor, classify(self, text, lang) function
     # or with setter.
-    def classify(self, vector, lang=None):
+    def classify(self, vector, lang, rubr_id):
         if lang:
-            if self.lang != lang:
-                self.lang = lang
+            if self.last_lang != lang or self.last_rubr_id != rubr_id:
+                self.last_lang = lang
+                self.last_rubr_id = rubr_id
                 self.loadClf()
         if self.clf:
             if self.clf.coef_.T.shape[0] == len(vector):
@@ -38,7 +39,8 @@ class Classifier(Module):
     
     def loadClf(self):
         file = os.path.join(os.path.dirname(__file__),
-                            self.config.get('Settings', '{0}_{1}'.format(self.rubr_id, self.lang)))
+                            self.config.get('Settings',
+                                            '{0}_{1}'.format(self.last_rubr_id, self.last_lang)))
         if os.path.exists(file):
             clf = joblib.load(file)
         else:
@@ -48,7 +50,7 @@ class Classifier(Module):
             
     # Language setter. Reloads classifier immediately.
     def setLang(self, lang):
-        self.lang = lang
+        self.last_lang = lang
         self.loadClf()
 
     def getLang(self):
