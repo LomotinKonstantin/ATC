b373e310c6583681d2a71577561b1994b1795e87
Konstantin Lomotin <ke.lomotin@gmail.com>
2019-04-12 02:41:05 +0300

    Сервер отлажен
    Signed-off-by: Konstantin Lomotin <ke.lomotin@gmail.com>

--- ATC/analyzer/modules/server.py
+++ ATC/analyzer/modules/server.py
@@ -1,8 +1,10 @@
 import socket
 import json
+import time
 
 
 TIMEOUT = 10
+BUFSIZE = 16384
 
 
 def is_valid_json(string: str) -> bool:
@@ -19,7 +21,6 @@ def null_terminate(err_gen_func):
         return res + "\0"
     return nt_err
 
-
 # {
 #     id: "идентификатор текста",
 #     title: "заголовок текста",
@@ -28,6 +29,8 @@ def null_terminate(err_gen_func):
 #     rubricator: "одно из ipv, grnti или subj",
 #     language: "одно из en, ru или auto",
 # }
+
+
 @null_terminate
 def validate_request_data(json_data: dict, analyzer) -> str:
     """
@@ -69,43 +72,66 @@ def start_server(port: int, analyzer) -> None:
         return
     connection, address = server_socket.accept()
     # 10-second timeout
-    connection.settimeout(TIMEOUT)
+    # connection.settimeout(TIMEOUT)
     print("Accepted connection from {}".format(address))
     while True:
-        # Receiving data
-        msg = ""
         try:
-            data = connection.recv(1024)
-            while not is_valid_json(msg):
-                msg += data.decode("utf-8")
-                data = connection.recv(1024)
-        except TimeoutError:
-            print("Client request timeout ({}s)".format(TIMEOUT))
+            continue_flag = False
+            # Receiving data
+            msg = connection.recv(BUFSIZE)
+            t = time.time()
+            while not is_valid_json(msg.decode("utf-8")):
+                data = connection.recv(BUFSIZE)
+                if not data:
+                    print("Connection closed, listening for new connection")
+                    connection, address = server_socket.accept()
+                    print("Accepted connection from {}".format(address))
+                    continue_flag = True
+                    break
+                msg += data
+                duration = time.time() - t
+                if duration > TIMEOUT:
+                    if msg:
+                        print("Timeout occurred during receiving request. Interrupting")
+                        continue_flag = True
+                        break
+            if continue_flag:
+                continue
+            msg = msg.decode()
+            # print("Received")
+            # Validating data
+            json_data = json.loads(msg, encoding="utf-8")
+            err_msg = validate_request_data(json_data, analyzer)
+            if err_msg != "\0":
+                # print("Error occurred:", err_msg)
+                # print("Sending error message")
+                connection.sendall(err_msg.encode("utf-8"))
+                continue
 
-        # Validating data
-        json_data = json.loads(msg, encoding="utf-8")
-        err_msg = validate_request_data(json_data, analyzer)
-        if err_msg:
-            connection.sendall(bytes(err_msg, encoding="utf-8"))
-            continue
-
-        # Processing data
-        text = " ".join([json_data.get("title", ""),
-                         " ".join(json_data.get("keywords", [])),
-                         json_data["body"]])
-        params = {
-            "language": json_data["language"],
-            "format": "auto",
-            "rubricator_id": json_data["rubricator"],
-        }
-        result = analyzer.analyze(text, params)
-        if not result:
-            connection.sendall(bytes("Unknown error occurred\0", encoding="utf-8"))
-            continue
-        proba_series = result.loc[0, "result"]
-        if proba_series is None:
-            connection.sendall(bytes("Classifier has rejected the text\0", encoding="utf-8"))
-            continue
-        proba_dict = proba_series.to_dict()
-        json_response_string = json.dumps(proba_dict)
-        connection.sendall(bytes(json_response_string, encoding="utf-8"))
+            # Processing data
+            text = " ".join([json_data.get("title", ""),
+                             " ".join(json_data.get("keywords", [])),
+                             json_data["body"]])
+            params = {
+                "language": json_data["language"],
+                "format": "auto",
+                "rubricator_id": json_data["rubricator"],
+            }
+            # print("Starting analyzer")
+            result = analyzer.analyze(text, params)
+            if not result:
+                connection.sendall(bytes("Unknown error occurred\0", encoding="utf-8"))
+                continue
+            proba_series = result.getPredict().loc[0, "result"]
+            if proba_series is None:
+                connection.sendall(bytes("Classifier has rejected the text\0", encoding="utf-8"))
+                continue
+            proba_dict = proba_series.to_dict()
+            json_response_string = json.dumps(proba_dict)
+            # print("Sending response")
+            # print(json_response_string)
+            connection.sendall(json_response_string.encode("utf-8"))
+            # print("Cycle done")
+        except ConnectionError:
+            print("Connection aborted, listening for new connection")
+            connection, address = server_socket.accept()
