565463b4be9d6da6773b3b67ca9619e73c42991e
Konstantin Lomotin <ke.lomotin@gmail.com>
2020-11-02 01:57:26 +0300

    Рефактор векторайзера

--- ATC/analyzer/modules/word_embedding/interface.py
+++ ATC/analyzer/modules/word_embedding/interface.py
@@ -4,7 +4,6 @@ from gensim.models import Word2Vec
 from configparser import ConfigParser
 import numpy as np
 import os.path
-import sys 
 from analyzer.modules.module import Module
 
 
@@ -14,13 +13,13 @@ class WordEmbedding(Module):
         self.lang = lang
         self.config = self.loadConfig()
         self.loadModel()
-        
+
     # If lang == None, creates vector according to last set language.
     # If lang != None, checks if input language corresponds with model language 
     # before creating vectors. If it does not, set new model language and changes model.
     # Language can be set in constructor, vectorize(self, text, lang) function
     # or with setter.
-    def vectorize(self, text, lang = None):
+    def vectorize(self, text, lang=None):
         if lang:
             if self.lang != lang:
                 self.lang = lang
@@ -32,7 +31,7 @@ class WordEmbedding(Module):
                 for t in tokens:
                     if t in self.model:
                         features += self.model[t]
-            elif self.config.get('Settings', 'convolution') in ['max','mean']:
+            elif self.config.get('Settings', 'convolution') in ['max', 'mean']:
                 for t in tokens:
                     if t in self.model:
                         features = np.vstack((features, self.model[t]))
@@ -55,13 +54,13 @@ class WordEmbedding(Module):
             self.model = None
 
     def loadConfig(self): 
-        configParcer = ConfigParser()
+        config_parser = ConfigParser()
         file = os.path.dirname(__file__) + '/config.ini'
         if os.path.exists(file):
-            configParcer.read(file)
+            config_parser.read(file)
         else:
             self.error_occurred.emit("Can't find the configuration file.")
-        return configParcer
+        return config_parser
 
     def rejectThreshold(self):
         return self.config.get("Settings", "reject_threshold")
