eb95277d034c4d4b04e336c9f0a6f4e25adea561
Konstantin Lomotin <ke.lomotin@gmail.com>
2020-11-02 01:15:51 +0300

    Рефактор загрузки классификатора

--- ATC/analyzer/modules/classifier/interface.py
+++ ATC/analyzer/modules/classifier/interface.py
@@ -1,5 +1,4 @@
 from configparser import ConfigParser
-import sys
 import os.path
 import pandas as pd
 from analyzer.modules.module import Module
@@ -7,31 +6,52 @@ import joblib
 from sklearn.multiclass import OneVsRestClassifier
 
 
+def read_model_and_md(path: str) -> tuple:
+    assert os.path.exists(path)
+    with open(path, "rb") as f:
+        model = joblib.load(f)
+        md = None
+        try:
+            md = joblib.load(f)
+        except EOFError:
+            pass
+    return model, md
+
+
 class Classifier(Module):
     def __init__(self, rubr_id='SUBJ', lang='ru'):
+        self.clf = None
+        self.clf_metadata = None
+        self.experiment_info = None
         super().__init__(os.path.dirname(__file__) + "\\metadata.json")
         self.rubr_id = rubr_id
         self.lang = lang
         self.config = self.loadConfig()
         self.loadClf()
-        #self.DEBUG = True
-        
-    # If lang == None, classifies vector according to last set language.
-    # If lang != None, checks if input language corresponds with current language 
-    # before classification. If it does not, set new current language and changes classifier.       
-    # Language can be set in constructor, classify(self, text, lang) function
-    # or with setter.
+        # self.DEBUG = True
+        # Сейчас загружена метадата модели, можем выводить ее пользователю
+        if self.clf_metadata is not None:
+            for k, v in self.clf_metadata.items():
+                if isinstance(v, dict):
+                    continue
+                self.metadata[k] = v
+            self.experiment_info = self.clf_metadata["settings"]
+
+
+    # Если lang == None, использует последний установленный язык.
+    # Иначе обновляет текущий язык.
     def classify(self, vector, lang=None, rubr_id=None):
-        if lang:
+        if lang is not None:
             if self.lang != lang:
                 self.lang = lang
                 self.loadClf()
-        if rubr_id:
+        if rubr_id is not None:
             if self.rubr_id != rubr_id:
                 self.rubr_id = rubr_id
                 self.loadClf()
-        if self.clf:
-            if (not hasattr(self.clf, 'coef_')) or (hasattr(self.clf, 'coef_') and self.clf.coef_.T.shape[0] == len(vector)):
+        if self.clf is not None:
+            has_good_coef_vec = hasattr(self.clf, 'coef_') and self.clf.coef_.T.shape[0] == len(vector)
+            if (not hasattr(self.clf, 'coef_')) or has_good_coef_vec:
                 if type(self.clf) == OneVsRestClassifier:
                     res = []
                     for i in self.clf.estimators_:
@@ -41,30 +61,28 @@ class Classifier(Module):
                 else:
                     result = pd.Series(self.clf.predict_proba([vector])[0], index=self.clf.classes_)
                     result = result.sort_values(ascending=False)
-                # result = result.round(3)
-                # result = result[result!=0]
-                return result   
+                return result
             else:
-                self.error_occurred.emit('Vector has '+str(len(vector))+' elements. Model can work with vectors that have '+str(self.clf.coef_.T.shape[0])+'attibutes.')
+                err_msg = f'Input vector has len {len(vector)}. Model requires {self.clf.coef_.T.shape[0]}.'
+                self.error_occurred.emit(err_msg)
                 return None
         else:
-            return None   
-    
-     
+            return None
+
     def loadConfig(self): 
-        configParcer = ConfigParser()
+        config_parser = ConfigParser()
         file = os.path.dirname(__file__) + '/config.ini'
         if os.path.exists(file):
-            configParcer.read(file)
+            config_parser.read(file)
         else:
             self.error_occurred.emit("Can't find the configuration file")
-        return configParcer    
+        return config_parser
     
     def loadClf(self):
         file = os.path.join(os.path.dirname(__file__),
                             self.config.get('Settings', self.rubr_id+'_'+self.lang))
         if os.path.exists(file):
-            self.clf = joblib.load(file)
+            self.clf, self.clf_metadata = read_model_and_md(file)
         else:
             self.error_occurred.emit("The classifier does not exist.")
             self.clf = None
